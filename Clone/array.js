592398c21cf940c5cf56ecc88c853fee5050d44b
Henrique Delacth <pintodelacthmendes16@hotmail.com>
2021-09-09 14:46:18 -0300

    Firts Upgrade

---
+++ Clone/array.js
@@ -0,0 +1,28 @@
+console.log(
+    ['Sheick', 'Spike', 'Rufus', 'Browni', 'Akamaru']
+)
+
+
+
+let animal = ['Dog', 'Cat', 'Monkey', 'Lion']
+
+for (let index = 0; index < animal.length; index++) {
+
+    let cont = index
+    
+    console.log(`${cont += 1}`,animal[index])
+}
+
+
+    let computer = [
+        'HP',
+        'Lenovo',
+        {
+            name : 'Samsung',
+            quantidade : 20,
+            
+        },
+        'Dell'
+    ]
+
+    console.log(computer[2].quantidade)
---
+++ Clone/object.js
@@ -0,0 +1,39 @@
+
+/*
+
+
+console.log({
+    name : 'Henrique',
+    idade : 17,
+    Skills: function(){console.log('JavaScript')}
+})
+
+
+
+console.log(
+    {
+        Akamaru : 3,
+        Rufus   : 0.8,
+        Sheick  : 0.8,
+        Spike   : 0.8,
+        Browni  : 0.5
+
+    }
+
+    
+)
+
+var object_dog = 1.8
+
+console.log( typeof object_dog)
+*/
+
+
+const pessoa ={
+    name : 'Henrique',
+    age  : 18,
+    weigth: 90,
+    isProgrammer : true
+}
+
+console.log(`${pessoa.name} tem ${pessoa.age} anos`)
